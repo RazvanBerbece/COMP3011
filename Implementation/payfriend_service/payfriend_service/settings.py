7af053f39f9cfb3a96d1c050e353650a84f1b0c0
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-03-28 23:09:28 +0100

    Update Django proj setting

--- Implementation/payfriend_service/payfriend_service/settings.py
+++ Implementation/payfriend_service/payfriend_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n%=it)e_q_$!zfo3i#)&j)5u3-otezk_tjck7!zpv*f^zri075
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 APPEND_SLASH = False
 
