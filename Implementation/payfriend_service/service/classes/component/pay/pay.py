9d0ca846b361303c20fcdc7bfa6a4c8960a14314
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-03-28 02:16:02 +0100

    add stub for payment service

---
+++ Implementation/payfriend_service/service/classes/component/pay/pay.py
@@ -0,0 +1,7 @@
+from ...contexts.transactions.transactions_context import *
+
+class PaymentService():
+    """
+    Interface which provides methods for processing payments, generating transactions and deleting transactions
+    and other utils
+    """
